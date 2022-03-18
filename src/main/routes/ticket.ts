b8ccbd70e9169fc1567c5aa096ad107eb1b10751
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-18 04:17:09 -0300

    feat: add finish ticket route

--- src/main/routes/ticket.ts
+++ src/main/routes/ticket.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { adaptRouter } from '../adapters/express-controller'
 import { makeCreateTicketController } from '../factories/controllers/create-ticket'
+import { makeFinishTicketController } from '../factories/controllers/finish-ticket'
 import { makeSuportTicketController } from '../factories/controllers/suport-ticket'
 import { verifyIsSuportLogin } from '../middlewares/verify-is-suport-login'
 import { verifyLogin } from '../middlewares/verify-login'
@@ -8,4 +9,5 @@ import { verifyLogin } from '../middlewares/verify-login'
 export default (router: Router): void => {
   router.post('/create-ticket', verifyLogin, adaptRouter(makeCreateTicketController()))
   router.patch('/suport-ticket', verifyLogin, verifyIsSuportLogin, adaptRouter(makeSuportTicketController()))
+  router.patch('/finish-ticket', verifyLogin, verifyIsSuportLogin, adaptRouter(makeFinishTicketController()))
 }
