0d0ed4cdaa813915bd1c68c61fb03584b45e3721
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-17 22:11:22 -0300

    feat: add express middleware adapter

---
+++ src/main/adapters/express-middleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express'
+import { Controller } from '../../presentation/contracts/controller'
+import { HttpRequest } from '../../presentation/contracts/http'
+
+export const adaptMiddleware = (middleware: Controller) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest: HttpRequest = {
+      headers: req.headers
+    }
+
+    const httpResponse = await middleware.handle(httpRequest)
+    if (httpResponse.statusCode === 200) {
+      Object.assign(req.body, httpResponse.data)
+      next()
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.data.message
+      })
+    }
+  }
+}
