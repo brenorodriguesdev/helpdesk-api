429686b611038bd696f67febebb7644723c094ef
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-18 16:52:59 -0300

    feat: add gets all by on ticket repository contract

--- src/data/contracts/ticket-repository.ts
+++ src/data/contracts/ticket-repository.ts
@@ -4,4 +4,7 @@ export interface TicketRepository {
   create: (ticket: Ticket) => Promise<Ticket>
   findById: (id: number) => Promise<Ticket>
   update: (ticket: Ticket) => Promise<void>
+  getAllByLoginClient: (idLoginClient) => Promise<Ticket[]>
+  getAllByLoginSuport: (idLoginSuport) => Promise<Ticket[]>
+  getAllByTicketStatus: (idTicketStatus) => Promise<Ticket[]>
 }
