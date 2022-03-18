78ac5e74b45b423d89b6b039409cd98a2dfc3aff
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-18 15:48:15 -0300

    feat: add delegate ticket route

--- src/main/routes/ticket.ts
+++ src/main/routes/ticket.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { adaptRouter } from '../adapters/express-controller'
 import { makeCreateTicketController } from '../factories/controllers/create-ticket'
+import { makeDelegateTicketController } from '../factories/controllers/delegate-ticket'
 import { makeFinishTicketController } from '../factories/controllers/finish-ticket'
 import { makeSuportTicketController } from '../factories/controllers/suport-ticket'
 import { verifyIsSuportLogin } from '../middlewares/verify-is-suport-login'
@@ -10,4 +11,5 @@ export default (router: Router): void => {
   router.post('/create-ticket', verifyLogin, adaptRouter(makeCreateTicketController()))
   router.patch('/suport-ticket', verifyLogin, verifyIsSuportLogin, adaptRouter(makeSuportTicketController()))
   router.patch('/finish-ticket', verifyLogin, verifyIsSuportLogin, adaptRouter(makeFinishTicketController()))
+  router.patch('/delegate-ticket', verifyLogin, verifyIsSuportLogin, adaptRouter(makeDelegateTicketController()))
 }
