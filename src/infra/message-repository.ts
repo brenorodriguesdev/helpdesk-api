112bf72e820e9056c172815319e4380ad207931e
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-18 12:29:04 -0300

    feat: implements updateStatusByTicketAndNotLogin on message repository pg

--- src/infra/message-repository.ts
+++ src/infra/message-repository.ts
@@ -6,4 +6,8 @@ export class MessageRepositoryPG implements MessageRepository {
   async create (message: Message): Promise<void> {
     await database.none('insert into message (body, createat, idticket, idmessagestatus, idloginsend) values ($1, $2, $3, $4, $5)', [message.body, message.createAt, message.ticket.id, message.messageStatus.id, message.loginSend.id])
   }
+
+  async updateStatusByTicketAndNotLogin (idMessageStatus: number, idLoginReceive: number, idNewMessageStatus: number): Promise<void> {
+    await database.none('update message set idmessagestatus = $1 where idloginsend <> $2 and idmessagestatus = $3', [idNewMessageStatus, idLoginReceive, idMessageStatus])
+  }
 }
