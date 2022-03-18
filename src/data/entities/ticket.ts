6317e5572d935413c87f7dbc5d3bc1f4fb54f588
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-18 00:38:13 -0300

    feat: add ticket entity

---
+++ src/data/entities/ticket.ts
@@ -0,0 +1,12 @@
+import { Login } from './login'
+import { TicketStatus } from './ticket-status'
+
+export interface Ticket {
+  id?: number
+  subject: string
+  createAt: Date
+  updateAt: Date
+  loginClient: Login
+  loginSuport: Login
+  ticketStatus: TicketStatus
+}
