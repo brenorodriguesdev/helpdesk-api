74509b4de45c9994e6edd90423ac950bc900d60a
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-18 12:31:59 -0300

    feat: add receive messages validator

---
+++ src/main/factories/validators/receive-messages.ts
@@ -0,0 +1,12 @@
+import { Validator } from '../../../validation/contracts/validator'
+import { CompositeValidator } from '../../../validation/validators/composite'
+import { RequiredFieldValidator } from '../../../validation/validators/required-field'
+
+export const makeReceiveMessagesValidator = (): Validator => {
+  const requiredFields = ['login', 'idTicket']
+  const validators = []
+  for (const requiredField of requiredFields) {
+    validators.push(new RequiredFieldValidator(requiredField))
+  }
+  return new CompositeValidator(validators)
+}
