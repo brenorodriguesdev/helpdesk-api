21bd9e6676fc8d71730a034b0a2fbe4164652732
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-18 12:34:53 -0300

    feat: add read messages route

--- src/main/routes/message.ts
+++ src/main/routes/message.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { adaptRouter } from '../adapters/express-controller'
+import { makeReadMessagesController } from '../factories/controllers/read-messages'
 import { makeSendMessageController } from '../factories/controllers/send-message'
 import { verifyLogin } from '../middlewares/verify-login'
 
 export default (router: Router): void => {
   router.post('/send-message', verifyLogin, adaptRouter(makeSendMessageController()))
+  router.put('/read-messages', verifyLogin, adaptRouter(makeReadMessagesController()))
 }
