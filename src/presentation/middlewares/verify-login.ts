5593b2f7c72186537085c7916ca007e90080b8f3
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-17 21:49:18 -0300

    feat: add verify login middleware

---
+++ src/presentation/middlewares/verify-login.ts
@@ -0,0 +1,20 @@
+import { VerifyLoginUseCase } from '../../domain/useCases/verify-login'
+import { HttpRequest, HttpResponse } from '../contracts/http'
+import { ok, serverError, unauthorized } from '../contracts/http-helper'
+import { Middleware } from '../contracts/middleware'
+
+export class VerifyLoginMiddleware implements Middleware {
+  constructor (private readonly verifyLoginUseCase: VerifyLoginUseCase) { }
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { accessToken } = httpRequest.body
+      const login = await this.verifyLoginUseCase.verify(accessToken)
+      if (login instanceof Error) {
+        return unauthorized(login)
+      }
+      return ok({ login })
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
