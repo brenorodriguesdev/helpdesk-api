4ac2b43ef1bee5b5bf2fd41d3f81b8ab2d5afcfb
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-18 04:15:03 -0300

    feat: add finish ticket controller

---
+++ src/presentation/controllers/finish-ticket.ts
@@ -0,0 +1,26 @@
+import { FinishTicketUseCase } from '../../domain/useCases/finish-ticket'
+import { Validator } from '../../validation/contracts/validator'
+import { Controller } from '../contracts/controller'
+import { HttpRequest, HttpResponse } from '../contracts/http'
+import { badRequest, noContent, serverError } from '../contracts/http-helper'
+
+export class FinishTicketController implements Controller {
+  constructor (private readonly validator: Validator, private readonly finishTicketUseCase: FinishTicketUseCase) { }
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const error = this.validator.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const { idTicket } = httpRequest.body
+      const result = await this.finishTicketUseCase.finish(idTicket)
+      if (result instanceof Error) {
+        return badRequest(result)
+      }
+      return noContent()
+    } catch (error) {
+      console.log(error.message)
+      return serverError()
+    }
+  }
+}
