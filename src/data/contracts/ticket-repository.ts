1e656a319f56cee44a192be85bd8011fd2bae27d
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-18 01:45:51 -0300

    feat: add ticket repository update contract

--- src/data/contracts/ticket-repository.ts
+++ src/data/contracts/ticket-repository.ts
@@ -3,4 +3,5 @@ import { Ticket } from '../entities/ticket'
 export interface TicketRepository {
   create: (ticket: Ticket) => Promise<Ticket>
   findById: (id: number) => Promise<Ticket>
+  update: (ticket: Ticket) => Promise<void>
 }
