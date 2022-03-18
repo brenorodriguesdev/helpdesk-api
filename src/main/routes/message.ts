3548389caf1a3d52d8ed2f234965e09d57e6e4d0
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-18 12:35:16 -0300

    feat: add receive messages route

--- src/main/routes/message.ts
+++ src/main/routes/message.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { adaptRouter } from '../adapters/express-controller'
 import { makeReadMessagesController } from '../factories/controllers/read-messages'
+import { makeReceiveMessagesController } from '../factories/controllers/receive-messages'
 import { makeSendMessageController } from '../factories/controllers/send-message'
 import { verifyLogin } from '../middlewares/verify-login'
 
 export default (router: Router): void => {
   router.post('/send-message', verifyLogin, adaptRouter(makeSendMessageController()))
+  router.put('/receive-messages', verifyLogin, adaptRouter(makeReceiveMessagesController()))
   router.put('/read-messages', verifyLogin, adaptRouter(makeReadMessagesController()))
 }
