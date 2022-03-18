c9bacee02bc08121d7f55a63e352bfcbee8d3a4b
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-18 03:29:58 -0300

    feat: implements findById and update on ticket repository pg

--- src/infra/ticket-repository-pg.ts
+++ src/infra/ticket-repository-pg.ts
@@ -17,4 +17,26 @@ export class TicketRepositoryPG implements TicketRepository {
     delete ticketModel.idticketstatus
     return ticketModel
   }
+
+  async findById (id: number): Promise<Ticket> {
+    const ticketTable = await database.oneOrNone('select * from ticket where id = $1', [id])
+    if (!ticketTable) {
+      return null
+    }
+    const idLoginClient = ticketTable.idloginclient
+    const loginClient = await database.oneOrNone('select * from login where id = $1', [idLoginClient])
+    const idLoginSuport = ticketTable.idloginsuport
+    const loginSuport = await database.oneOrNone('select * from login where id = $1', [idLoginSuport])
+    const idTicketStatus = ticketTable.idticketstatus
+    const ticketStatus = await database.oneOrNone('select * from ticketstatus where id = $1', [idTicketStatus])
+    const ticketModel = { ...ticketTable, loginClient, loginSuport, ticketStatus }
+    delete ticketModel.idloginclient
+    delete ticketModel.idloginsuport
+    delete ticketModel.idticketstatus
+    return ticketModel
+  }
+
+  async update (ticket: Ticket): Promise<void> {
+    await database.none('update ticket set subject = $1, createat = $2, updateat = $3, idloginclient = $4, idloginsuport = $5, idticketstatus = $6 where id = $7', [ticket.subject, ticket.createAt, ticket.updateAt, ticket.loginClient.id, ticket.loginSuport.id, ticket.ticketStatus.id, ticket.id])
+  }
 }
