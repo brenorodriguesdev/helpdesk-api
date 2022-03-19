820d2a926ea07358902632b4d39874b1b1f68cf4
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-19 19:07:00 -0300

    feat: call notifyMessage on readMessages

--- src/data/services/read-messages.ts
+++ src/data/services/read-messages.ts
@@ -1,4 +1,5 @@
 import { ReadMessagesModel } from '../../domain/models/read-messages'
+import { NotifyMessageUseCase } from '../../domain/useCases/notify-message'
 import { ReadMessagesUseCase } from '../../domain/useCases/read-messages'
 import { LoginRepository } from '../contracts/login-repository'
 import { MessageRepository } from '../contracts/message-repository'
@@ -7,7 +8,8 @@ import { TicketRepository } from '../contracts/ticket-repository'
 export class ReadMessagesService implements ReadMessagesUseCase {
   constructor (private readonly ticketRepository: TicketRepository,
     private readonly loginRepository: LoginRepository,
-    private readonly messageRepository: MessageRepository) {}
+    private readonly messageRepository: MessageRepository,
+    private readonly notifyMessage: NotifyMessageUseCase) {}
 
   async read ({ idTicket, idLoginRead }: ReadMessagesModel): Promise<void | Error> {
     const ticket = await this.ticketRepository.findById(idTicket)
@@ -20,6 +22,14 @@ export class ReadMessagesService implements ReadMessagesUseCase {
     }
     const messageStatusSent = 2
     const messageStatusRead = 4
+    const messagesRead = await this.messageRepository.getByTicketAndNotLogin(messageStatusSent, loginReceive.id)
     await this.messageRepository.updateStatusByTicketAndNotLogin(messageStatusSent, loginReceive.id, messageStatusRead)
+    const idLogin = loginReceive.id === ticket.loginClient.id ? ticket.loginSuport.id : ticket.loginClient.id
+    for (const messageRead of messagesRead) {
+      await this.notifyMessage.notify({
+        idLogin,
+        message: messageRead
+      })
+    }
   }
 }
