7247a9270ed457ddaf3845e8973eebf38ed34568
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-19 18:02:48 -0300

    feat: add getStatusByTicketAndNotLogin on message repository contract

--- src/data/contracts/message-repository.ts
+++ src/data/contracts/message-repository.ts
@@ -2,6 +2,7 @@ import { Message } from '../entities/message'
 
 export interface MessageRepository {
   create: (message: Message) => Promise<void>
+  getStatusByTicketAndNotLogin: (idMessageStatus: number, idLoginReceive: number) => Promise<Message[]>
   updateStatusByTicketAndNotLogin: (idMessageStatus: number, idLoginReceive: number, idNewMessageStatus: number) => Promise<void>
   getByTicket: (idTicket: number) => Promise<Message[]>
 }
