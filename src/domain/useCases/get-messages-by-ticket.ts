8b6620480e6b41eeb2a632acd6941ffc094430e5
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-19 16:24:02 -0300

    feat: add get messages by ticket usecase

---
+++ src/domain/useCases/get-messages-by-ticket.ts
@@ -0,0 +1,6 @@
+import { GetMessagesByTicketModel } from '../models/get-messages-by-ticket'
+import { MessageModel } from '../models/message'
+
+export interface GetMessagesByTicketUseCase {
+  get: (data: GetMessagesByTicketModel) => Promise<MessageModel[]>
+}
