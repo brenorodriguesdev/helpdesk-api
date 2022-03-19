11f53e1a61290552dbc60ce90a0dfb442d534bf4
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-19 17:03:08 -0300

    feat: add get messages by ticket route

--- src/main/routes/message.ts
+++ src/main/routes/message.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { adaptRouter } from '../adapters/express-controller'
+import { makeGetMessagesByTicketController } from '../factories/controllers/get-messages-by-ticket'
 import { makeReadMessagesController } from '../factories/controllers/read-messages'
 import { makeReceiveMessagesController } from '../factories/controllers/receive-messages'
 import { makeSendMessageController } from '../factories/controllers/send-message'
@@ -9,4 +10,5 @@ export default (router: Router): void => {
   router.post('/send-message', verifyLogin, adaptRouter(makeSendMessageController()))
   router.put('/receive-messages', verifyLogin, adaptRouter(makeReceiveMessagesController()))
   router.put('/read-messages', verifyLogin, adaptRouter(makeReadMessagesController()))
+  router.get('/get-messages-by-ticket/:idTicket', verifyLogin, adaptRouter(makeGetMessagesByTicketController()))
 }
