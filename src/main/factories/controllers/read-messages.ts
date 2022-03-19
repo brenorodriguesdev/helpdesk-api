13e0f17c72530114c8eaba554743e8b8dd7ea454
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-19 19:10:24 -0300

    fix: inject notifyMessage on read messages and receive messages

--- src/main/factories/controllers/read-messages.ts
+++ src/main/factories/controllers/read-messages.ts
@@ -1,7 +1,9 @@
+import { NotifyMessageService } from '../../../data/services/notify-message'
 import { ReadMessagesService } from '../../../data/services/read-messages'
 import { LoginRepositoryPG } from '../../../infra/login-repository-pg'
 import { MessageRepositoryPG } from '../../../infra/message-repository'
 import { TicketRepositoryPG } from '../../../infra/ticket-repository-pg'
+import { UserConnectionSocket } from '../../../infra/user-connection-socket'
 import { ReadMessagesController } from '../../../presentation/controllers/read-messages'
 import { makeReadMessagesValidator } from '../validators/read-messages'
 
@@ -9,6 +11,8 @@ export const makeReadMessagesController = (): ReadMessagesController => {
   const loginRepositoryPG = new LoginRepositoryPG()
   const ticketRepositoryPG = new TicketRepositoryPG()
   const messageRepositoryPG = new MessageRepositoryPG()
-  const readMessagesService = new ReadMessagesService(ticketRepositoryPG, loginRepositoryPG, messageRepositoryPG)
+  const userConnectionSocket = new UserConnectionSocket()
+  const notifyMessageService = new NotifyMessageService(userConnectionSocket)
+  const readMessagesService = new ReadMessagesService(ticketRepositoryPG, loginRepositoryPG, messageRepositoryPG, notifyMessageService)
   return new ReadMessagesController(makeReadMessagesValidator(), readMessagesService)
 }
--- src/main/factories/controllers/receive-messages.ts
+++ src/main/factories/controllers/receive-messages.ts
@@ -1,7 +1,9 @@
+import { NotifyMessageService } from '../../../data/services/notify-message'
 import { ReceiveMessagesService } from '../../../data/services/receive-messages'
 import { LoginRepositoryPG } from '../../../infra/login-repository-pg'
 import { MessageRepositoryPG } from '../../../infra/message-repository'
 import { TicketRepositoryPG } from '../../../infra/ticket-repository-pg'
+import { UserConnectionSocket } from '../../../infra/user-connection-socket'
 import { ReceiveMessagesController } from '../../../presentation/controllers/receive-messages'
 import { makeReceiveMessagesValidator } from '../validators/receive-messages'
 
@@ -9,6 +11,8 @@ export const makeReceiveMessagesController = (): ReceiveMessagesController => {
   const loginRepositoryPG = new LoginRepositoryPG()
   const ticketRepositoryPG = new TicketRepositoryPG()
   const messageRepositoryPG = new MessageRepositoryPG()
-  const receiveMessagesService = new ReceiveMessagesService(ticketRepositoryPG, loginRepositoryPG, messageRepositoryPG)
+  const userConnectionSocket = new UserConnectionSocket()
+  const notifyMessageService = new NotifyMessageService(userConnectionSocket)
+  const receiveMessagesService = new ReceiveMessagesService(ticketRepositoryPG, loginRepositoryPG, messageRepositoryPG, notifyMessageService)
   return new ReceiveMessagesController(makeReceiveMessagesValidator(), receiveMessagesService)
 }
