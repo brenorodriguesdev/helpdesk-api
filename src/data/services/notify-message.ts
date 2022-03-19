f95c821683210d4effe943b81c94fc03bef5e626
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-19 18:05:28 -0300

    feat: add notify message service

---
+++ src/data/services/notify-message.ts
@@ -0,0 +1,10 @@
+import { NotifyMessageModel } from '../../domain/models/notify-message'
+import { NotifyMessageUseCase } from '../../domain/useCases/notify-message'
+import { UserConnection } from '../contracts/user-connection'
+
+export class NotifyMessageService implements NotifyMessageUseCase {
+  constructor (private readonly userConnection: UserConnection) {}
+  async notify ({ idLogin, message }: NotifyMessageModel): Promise<void> {
+    await this.userConnection.sendPacket(idLogin, message)
+  }
+}
