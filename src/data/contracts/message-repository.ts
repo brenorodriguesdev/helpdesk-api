958bd2a2619871c24d41f7831366449c0b02ecb1
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-19 16:26:18 -0300

    feat: add get by ticket on message repository contract

--- src/data/contracts/message-repository.ts
+++ src/data/contracts/message-repository.ts
@@ -3,4 +3,5 @@ import { Message } from '../entities/message'
 export interface MessageRepository {
   create: (message: Message) => Promise<void>
   updateStatusByTicketAndNotLogin: (idMessageStatus: number, idLoginReceive: number, idNewMessageStatus: number) => Promise<void>
+  getByTicket: (idTicket: number) => Promise<Message[]>
 }
