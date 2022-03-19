515a2dfc46eb99df6693a7cf518235dbcb89adb5
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-19 18:22:31 -0300

    fix: inject notifyMessageService on sendMessageService

--- src/main/factories/controllers/send-message.ts
+++ src/main/factories/controllers/send-message.ts
@@ -1,7 +1,9 @@
+import { NotifyMessageService } from '../../../data/services/notify-message'
 import { SendMessageService } from '../../../data/services/send-message'
 import { LoginRepositoryPG } from '../../../infra/login-repository-pg'
 import { MessageRepositoryPG } from '../../../infra/message-repository'
 import { TicketRepositoryPG } from '../../../infra/ticket-repository-pg'
+import { UserConnectionSocket } from '../../../infra/user-connection-socket'
 import { SendMessageController } from '../../../presentation/controllers/send-message'
 import { makeSendMessageValidator } from '../validators/send-message'
 
@@ -9,6 +11,8 @@ export const makeSendMessageController = (): SendMessageController => {
   const loginRepositoryPG = new LoginRepositoryPG()
   const ticketRepositoryPG = new TicketRepositoryPG()
   const messageRepositoryPG = new MessageRepositoryPG()
-  const sendMessageService = new SendMessageService(loginRepositoryPG, ticketRepositoryPG, messageRepositoryPG)
+  const userConnectionSocket = new UserConnectionSocket()
+  const notifyMessageService = new NotifyMessageService(userConnectionSocket)
+  const sendMessageService = new SendMessageService(loginRepositoryPG, ticketRepositoryPG, messageRepositoryPG, notifyMessageService)
   return new SendMessageController(makeSendMessageValidator(), sendMessageService)
 }
