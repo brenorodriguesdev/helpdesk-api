a14b830e574f1f4ba8bfb7d0c86506449f48f860
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-19 19:00:59 -0300

    feat: add on disconnect event

--- src/main/sockets/user-connection.ts
+++ src/main/sockets/user-connection.ts
@@ -1,7 +1,9 @@
 import { Socket } from 'socket.io'
 import { adaptEvent } from '../adapters/socket-controller'
 import { makeOnConnectController } from '../factories/controllers/on-connect'
+import { makeOnDisconnectController } from '../factories/controllers/on-disconnect'
 
 export default (socket: Socket): void => {
   socket.on('onConnect', async (packet: any) => await adaptEvent(socket, packet, makeOnConnectController()))
+  socket.on('disconnect', async (packet: any) => await adaptEvent(socket, packet, makeOnDisconnectController()))
 }
