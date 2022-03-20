3774c50ab705f27a68c4fc3d4d5cd9a3208f3a6c
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-20 15:14:16 -0300

    fix: get tickets in queue service status

--- src/data/services/get-tickets-in-queue.ts
+++ src/data/services/get-tickets-in-queue.ts
@@ -5,7 +5,7 @@ import { TicketRepository } from '../contracts/ticket-repository'
 export class GetTicketsInQueueService implements GetTicketsInQueueUseCase {
   constructor (private readonly ticketRepository: TicketRepository) {}
   async get (): Promise<TicketModel[]> {
-    const ticketInQueue = 2
+    const ticketInQueue = 1
     const tickets = await this.ticketRepository.getAllByTicketStatus(ticketInQueue)
     return tickets
   }
