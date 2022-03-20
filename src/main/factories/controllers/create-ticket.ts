43679b046daf001b996bd8858c63152a7379a55d
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-03-20 18:45:21 -0300

    fix: inject notify new ticket service on create ticket service

--- src/main/factories/controllers/create-ticket.ts
+++ src/main/factories/controllers/create-ticket.ts
@@ -1,7 +1,9 @@
 import { CreateTicketService } from '../../../data/services/create-ticket'
+import { NotifyNewTicketService } from '../../../data/services/notify-new-ticket'
 import { LoginRepositoryPG } from '../../../infra/login-repository-pg'
 import { MessageRepositoryPG } from '../../../infra/message-repository'
 import { TicketRepositoryPG } from '../../../infra/ticket-repository-pg'
+import { UserConnectionSocket } from '../../../infra/user-connection-socket'
 import { CreateTicketController } from '../../../presentation/controllers/create-ticket'
 import { makeCreateTicketValidator } from '../validators/create-ticket'
 
@@ -9,6 +11,8 @@ export const makeCreateTicketController = (): CreateTicketController => {
   const loginRepositoryPG = new LoginRepositoryPG()
   const ticketRepositoryPG = new TicketRepositoryPG()
   const messageRepositoryPG = new MessageRepositoryPG()
-  const createTicketService = new CreateTicketService(loginRepositoryPG, ticketRepositoryPG, messageRepositoryPG)
+  const userConnectionSocket = new UserConnectionSocket()
+  const notifyNewTicketService = new NotifyNewTicketService(userConnectionSocket)
+  const createTicketService = new CreateTicketService(loginRepositoryPG, ticketRepositoryPG, messageRepositoryPG, notifyNewTicketService)
   return new CreateTicketController(makeCreateTicketValidator(), createTicketService)
 }
